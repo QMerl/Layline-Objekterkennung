ab5621e69627018592adfd87283eab82510870a3
QMerl <quentinlagrange@outlook.de>
2024-08-26 09:51:03 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -38,7 +38,6 @@ class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs):
         super(CNNBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
-        self.batchnorm = nn.BatchNorm2d(out_channels)
         self.leakyrelu = nn.LeakyReLU(0.1)
 
     def forward(self, x):
